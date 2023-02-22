d6ba2ebb5c364d747745d27300de22c68bd5129c
nanda <nandawijaya@apps.ipb.ac.id>
2023-02-23 00:39:30 +0700

    add backend

--- app/src/main/java/com/example/appl/MainActivity.java
+++ app/src/main/java/com/example/appl/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.appl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText getUsername = (EditText) findViewById(R.id.username);
+        EditText getPassword = (EditText) findViewById(R.id.password);
+
+        String username = String.valueOf(getUsername.getText().toString());
+        String password = String.valueOf(getPassword.getText().toString());
+
+        Button btnLogin = (Button) findViewById(R.id.login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username != "admin" && password != "password"){
+                    startActivity(new Intent(MainActivity.this, MainActivity3.class));
+                }
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
     }
 }
